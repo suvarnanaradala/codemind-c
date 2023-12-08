00f48ce350bfe7a67d434eb1a769f8d7f0276d9c
suvarnanaradala <147792260+suvarnanaradala@users.noreply.github.com>
2023-12-08 17:56:22 +0530

    #include<stdio.h> int main() {     int r,n,count=0;     scanf("%d",&n);     while(n>0)     {         r=n%10;         count=count+1;         n=n/10;     }     printf("%d ",count); }

---
+++ Find_the_Digits_Count.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int r,n,count=0;
+    scanf("%d",&n);
+    while(n>0)
+    {
+        r=n%10;
+        count=count+1;
+        n=n/10;
+    }
+    printf("%d ",count);
+}
